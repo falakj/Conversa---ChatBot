ca81e327152e401f12a11a6aec590faa835bfbbf
Falak <46991207+falakj@users.noreply.github.com>
2023-08-09 18:45:30 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -13,12 +13,14 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(helmet({
+    crossOriginResourcePolicy: false,
+  }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors(
-        //origin: ["https://falaz-conversa-client-beta.vercel.app"],
+        origin: ["https://falaz-conversa-client-beta.vercel.app"],
         methods: ["POST", "GET"],
         credentials: true
 ));
