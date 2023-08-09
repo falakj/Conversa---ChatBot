f3df38c794b643eda0b8b4fe52e262e4e06eb931
Falak <46991207+falakj@users.noreply.github.com>
2023-08-09 19:01:27 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -13,9 +13,7 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(helmet());
-app.use(helmet({
-    crossOriginResourcePolicy: false,
-  }));
+app.use(helmet());
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
