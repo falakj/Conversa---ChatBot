b002761549b2730695a71c824b7c9916533d5d35
Falak <46991207+falakj@users.noreply.github.com>
2023-08-09 17:16:17 +0530

    Update

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+app.use(cors({ origin: true }));
 
 /* OPEN-AI CONFIGURATION */
 const configuration = new Configuration({
