4c8bceeb0d0b33bb9385dc5820e2d5d6539cffc2
Falak <46991207+falakj@users.noreply.github.com>
2023-08-09 17:18:13 +0530

    Update

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors({ origin: true }));
+app.use(cors({ origin: 'https://falaz-conversa-client-beta.vercel.app'}));
 
 /* OPEN-AI CONFIGURATION */
 const configuration = new Configuration({
